9dcb2d85c5a6dc3242b87e2ceaf915f6777154b1
Adam Venturella <aventurella@gmail.com>
2011-05-15 08:33:55 -0700

    cleanup

--- www/application/controllers/LoginController.php
+++ www/application/controllers/LoginController.php
@@ -6,28 +6,7 @@ class LoginController extends Controller
 {
 	protected function initialize_complete()
 	{
-		$this->messages = new stdClass();
-		$this->model = new stdClass();
-		$this->model->messages = "{}";
-		$this->model->formSuccess = false;
-		
-		
-		/*
-		$db = Database::connection(Database::kSql);
-		$query = new AMQuery($db);
-		$query->sql = "select * from user";
-		
-		foreach($query as $row)
-		{
-			echo $row["username"], " : ", $row["email"], "<br>";
-		}
-		
-		
-		if($this->isPostBack)
-		{
-			$this->processForm();
-		}
-		*/
+
 	}
 	
 	public function foo()
