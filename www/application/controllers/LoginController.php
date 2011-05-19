503177c0469db18f01bfac08aa78654c645e8451
Brian Puglisi <bpuglisi@gmail.com>
2011-05-18 23:04:37 -0500

    ..

--- www/application/controllers/LoginController.php
+++ www/application/controllers/LoginController.php
@@ -10,8 +10,29 @@ class LoginController extends System\Controller
 	public function index()
 	{
 		$this->model = new stdClass();
-		$this->model->message = "Hello World";
-		
+
+		if($this->isPostBack)
+		{
+			require System\Application::basePath().'/application/models/User.php';
+			require System\Application::basePath().'/application/api/AbstractApi.php';
+			require System\Application::basePath().'/application/api/Accounts.php';
+			require System\Application::basePath().'/application/libs/axismundi/forms/AMForm.php';
+			require System\Application::basePath().'/application/libs/axismundi/forms/validators/AMEmailValidator.php';
+			require System\Application::basePath().'/application/libs/axismundi/forms/validators/AMPatternValidator.php';
+			require System\Application::basePath().'/application/libs/axismundi/forms/validators/AMMatchValidator.php';
+			require System\Application::basePath().'/application/libs/axismundi/forms/validators/AMErrorValidator.php';
+			
+			$accounts = new Api\Accounts();
+			$response = $accounts->login();
+			
+			if($response->ok == true)
+			{
+				$this->redirect("/home");
+			}
+			
+			$this->model->messages = json_encode($response);
+		}
+	
 		return $this->view('login', $this->model);
 	}
 }
